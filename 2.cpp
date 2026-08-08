543d625b0455680a35b37802d2f70b3177bda0e4
harshrana14-fi <jatoliyaharsh8@gmail.com>
2026-08-08 10:14:57 +0530

    Programs

---
+++ 2.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+using namespace std;
+
+void insertionSort(int arr[], int n)
+{
+    for(int i = 1; i < n; i++)
+    {
+        int key = arr[i];
+        int j = i - 1;
+
+        while(j >= 0 && arr[j] > key)
+        {
+            arr[j + 1] = arr[j];
+            j--;
+        }
+
+        arr[j + 1] = key;
+    }
+}
+
+int main()
+{
+    cout << "Name : Harsh Jatoliya" << endl;
+    cout << "Enrollment No. : 13114803124" << endl << endl;
+
+    int n;
+    cout << "Enter number of elements: ";
+    cin >> n;
+
+    int arr[n];
+
+    cout << "Enter elements:\n";
+    for(int i = 0; i < n; i++)
+        cin >> arr[i];
+
+    insertionSort(arr, n);
+
+    cout << "Sorted Array using Insertion Sort: ";
+    for(int i = 0; i < n; i++)
+        cout << arr[i] << " ";
+
+    return 0;
+}
